e9fce42d57fc2d615d735410f01967213562229d
hidden-earth-77527 <68etop@gmail.com>
2018-01-18 11:25:15 +0800

    master

--- hello/views.py
+++ hello/views.py
@@ -6,8 +6,9 @@ import requests
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    r = requests.get('http://httpbin.org/status/418')
-    return HttpResponse('<pre>' + r.text + '</pre>')#render(request, 'index.html')
+    #r = requests.get('http://httpbin.org/status/418')
+    #print(r.text)
+    return render(request, 'index.html')#HttpResponse('<pre>' + r.text + '</pre>')
 
 
 def db(request):
